7047a422240a07a85322cdf3b2b5115593d9655f
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-09-07 19:01:05 -0500

    feat: Add validations with express-validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
 const router = Router();
+const { check } = require('express-validator');
 const { loginUser, renewToken, registerUser } = require('../controllers/auth');
+const { fieldsValidators } = require('../middlewares/fields-validator');
 
 router.get('/renew', renewToken)
 
-router.post('/', loginUser)
+router.post('/', [
+  check('email', 'Email should have correct format').isEmail(),
+  check('password', 'Password in not correct').isLength(5),
+  fieldsValidators
+], loginUser)
 
-router.post('/newUser', registerUser)
+router.post('/newUser', [
+  check('name', 'Name is mandatory').not().isEmpty(),
+  check('email', 'Email should have correct format').isEmail(),
+  check('password', 'Password should be 5 characters minimun').isLength(5),
+  fieldsValidators,
+],registerUser)
 
 module.exports = router;
