157d23988bf614f05f6837e94816b42ef6eb1e2e
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-09-10 19:47:50 -0500

    feat: Add cors because server rejected connect with front

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { dbConnection } = require('./database/config');
 
 dbConnection();
 
 const app = express();
+
+app.use(cors({
+  origin: "*"
+}));
 
 app.use( express.json() );
 
